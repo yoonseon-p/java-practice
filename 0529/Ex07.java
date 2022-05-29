2a04bd7963a0e77fd86d69ad8f0eef1269cde708
박윤선 <88888198+yoonseon-p@users.noreply.github.com>
2022-05-29 17:39:32 +0900

    Add files via upload

---
+++ 0529/Ex07.java
@@ -0,0 +1,14 @@
+package basic;
+//료형은 double 이고 요소수가 5인 배열을 생성, 모든 요소 출력
+public class Ex07 {
+
+	public static void main(String[] args) {
+		double[] doubleArr =new double[5];
+		
+		for(int i=0; i<doubleArr.length; i++) {
+			System.out.println(doubleArr[i]);
+		}
+
+	}
+
+}
