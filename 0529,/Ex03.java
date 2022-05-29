eb050183658296a1aacdc3c5bb8cd34220290eab
박윤선 <88888198+yoonseon-p@users.noreply.github.com>
2022-05-29 16:28:57 +0900

    Add files via upload

---
+++ 0529,/Ex03.java
@@ -0,0 +1,22 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("베열의 크기 지정 : ");
+		int n = in.nextInt();
+		
+		String[] grades =new String[n];
+		System.out.println(grades.length);
+		
+		for(int i=0; i<grades.length; i++) {
+			System.out.println(grades[i]);
+		}
+		
+		String[] strArr =new String[7];
+		
+	}
+
+}
---
+++ 0529,/Ex04.java
@@ -0,0 +1,14 @@
+package basic;
+
+import java.util.Scanner;
+
+public class Ex04 {
+	public static void main(String[] args) {
+		String[] names = {"Sam", "Kate", "John", "Jenny"};
+		
+		for(int i=0; i < names.length; i++) {
+			System.out.println(names[i]);
+		}
+		
+	}
+}
