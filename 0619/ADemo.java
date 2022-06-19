191460846fba7bddeb1fd91b08a5d6b7784ef286
박윤선 <88888198+yoonseon-p@users.noreply.github.com>
2022-06-19 17:54:02 +0900

    Add files via upload

---
+++ 0619/ADemo.java
@@ -0,0 +1,12 @@
+package main;
+
+import sample.A;
+
+public class ADemo {
+	public static void main(String[] args) {
+		A a = new A();
+		a.public_value_a = 10;
+		// a.protected_value_a = 10;
+		// a.value_a=10;
+	}
+}
---
+++ 0619/ExCarDemo.java
@@ -0,0 +1,20 @@
+package main;
+import product.ExCar;
+
+public class ExCarDemo {
+
+	public static void main(String[] args) {
+		ExCar ec = new ExCar("abc", 100, 100, 100, 150);
+		ec.getSpec();
+		ec.move(10,5);
+		ec.getSpec();
+		
+		
+//		SimpleDateFormat sd = new SimpleDateFormat("yyyy-mm-dd HH:mm:ss");
+//		SimpleDateFormat sd = new SimpleDateFormat("yyyy³â mm¿ù ddÀÏ");
+//		String myDate = sd.format(new Date());
+//		System.out.println(new Date());
+//		System.out.println(myDate);
+	}
+
+}
